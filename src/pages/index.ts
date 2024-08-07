2eea83b17e9ec58cb9d5e1487b9ab54ac573bd96
Christoph <christophschmid01@gmail.com>
2024-08-07 16:09:02 +0200

    feat(routing): Add new pages for authentication and main functionality

---
+++ src/pages/index.ts
@@ -0,0 +1,12 @@
+export { default as Home } from "./_main/Home";
+export { default as Chats } from "./_main/Chats";
+export { default as CreatePost } from "./_main/CreatePost";
+export { default as EditPost } from "./_main/EditPost";
+export { default as AllUsers } from "./_main/AllUsers";
+export { default as Profile } from "./_main/Profile";
+export { default as Explore } from "./_main/Explore";
+export { default as Notifications } from "./_main/Notifications";
+export { default as EditProfile } from "./_main/EditProfile";
+export { default as Stories } from "./_main/Stories";
+export { default as Reels } from "./_main/Reels";
+export { default as SavedPosts } from "./_main/SavedPosts";
