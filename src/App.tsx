6fddaa0d2bb7018a8bb1efdb74ed9900655752f6
Christoph <christophschmid01@gmail.com>
2024-08-07 17:11:11 +0200

    feat(routing): configure Router with MainLayout, AuthLayout, and routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,54 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  Home,
+  Chats,
+  CreatePost,
+  EditPost,
+  AllUsers,
+  Profile,
+  Explore,
+  Notifications,
+  EditProfile,
+  Stories,
+  Reels,
+  SavedPosts,
+  LogIn,
+  SignUp,
+} from "./pages";
+import MainLayout from "./layout/MainLayout";
+import AuthLayout from "./layout/AuthLayout";
+import ProtectedRoute from "./components/shared/ProtectedRoute";
+
 function App() {
+  const protectedRoutes = [
+    { path: "/", element: <Home /> },
+    { path: "/chats", element: <Chats /> },
+    { path: "/create-post", element: <CreatePost /> },
+    { path: "/edit-post/:id", element: <EditPost /> },
+    { path: "/all-users", element: <AllUsers /> },
+    { path: "/profile/:id", element: <Profile /> },
+    { path: "/explore", element: <Explore /> },
+    { path: "/notifications", element: <Notifications /> },
+    { path: "/edit-profile/:id", element: <EditProfile /> },
+    { path: "/stories", element: <Stories /> },
+    { path: "/reels", element: <Reels /> },
+    { path: "/saved-posts", element: <SavedPosts /> },
+  ];
+
   return (
-    <>
-      <h1 className="text-5xl">Starter Code</h1>
-    </>
+    <Router>
+      <Routes>
+        <Route element={<MainLayout />}>
+          {protectedRoutes.map((item, index) => (
+            <Route key={index} path={item.path} element={item.element} />
+          ))}
+        </Route>
+        <Route element={<AuthLayout />}>
+          <Route path="/log-in" element={<LogIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
+        </Route>
+      </Routes>
+    </Router>
   );
 }
 
