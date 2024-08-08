bd7d1414614c0cad8f1108a7402cb4db999574cb
Christoph <christophschmid01@gmail.com>
2024-08-08 14:45:25 +0200

    feat(styles): update tailwind.config.js with new styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,10 @@
 module.exports = {
   darkMode: ["class"],
   content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
   ],
   prefix: "",
   theme: {
@@ -13,10 +13,46 @@ module.exports = {
       center: true,
       padding: "2rem",
       screens: {
-        "2xl": "1400px",
+        "2xl": "1440px",
       },
     },
+    fontSize: {
+      xs: ["0.75rem", { lineHeight: "1.4" }],
+      sm: ["0.875rem", { lineHeight: "1.4" }],
+      base: ["1rem", { lineHeight: "1.4" }],
+      lg: ["1.125rem", { lineHeight: "1.4" }],
+      xl: ["1.25rem", { lineHeight: "1.4" }],
+      "2xl": ["1.5rem", { lineHeight: "1.4" }],
+      "3xl": ["1.875rem", { lineHeight: "1.4" }],
+    },
     extend: {
+      screens: {
+        xs: "480px",
+        "2xl": "1400px",
+      },
+      colors: {
+        primary: "hsl(244, 100%, 75%)",
+        secondary: "hsl(40, 100%, 56%)",
+        "dark-100": "hsl(0, 0%, 0%)",
+        "dark-200": "hsl(240, 5%, 4%)",
+        "dark-300": "hsl(240, 6%, 7%)",
+        "dark-400": "hsl(240, 5%, 13%)",
+        "dark-500": "hsl(240, 5%, 20%)",
+        "light-100": "hsl(0, 0%, 100%)",
+        "light-200": "hsl(0, 0%, 94%)",
+        "light-300": "hsl(240, 19%, 55%)",
+        "light-400": "hsl(240, 14%, 42%)",
+        red: "#FF5A5A",
+      },
+      fontFamily: {
+        inter: ["Inter", "sans-serif"],
+        body: ["Inter", "sans-serif"],
+        heading: ["Inter", "sans-serif"],
+      },
+      backgroundImage: {
+        "gradient-border":
+          "linear-gradient(180deg, #877EFF, #685DFF 46.1458%, #3121FF)",
+      },
       keyframes: {
         "accordion-down": {
           from: { height: "0" },
@@ -33,5 +69,6 @@ module.exports = {
       },
     },
   },
+
   plugins: [require("tailwindcss-animate")],
-}
+};
