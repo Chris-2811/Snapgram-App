66fbe22619ff3e8f46b7692d499e2699bf07e1b6
Christoph <christophschmid01@gmail.com>
2024-08-11 13:24:42 +0200

    feat(auth): Add IUser, IErrors, and ILoginErrors interfaces

---
+++ src/types/index.ts
@@ -0,0 +1,20 @@
+export interface IUser {
+  id: string;
+  email: string;
+  bio: string;
+  photoUrl: string;
+  username: string;
+  name: string;
+  createdAt: string;
+}
+
+export interface IErrors {
+  email: string;
+  password: string;
+  name: string;
+}
+
+export interface ILoginErrors {
+  email: string;
+  password: string;
+}
