999b8d9b14ecacf9aa532c5eb03df86136666956
Christoph <christophschmid01@gmail.com>
2024-08-11 14:40:51 +0200

    feat(auth): Update input component to signal error

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -3,15 +3,22 @@ import * as React from "react";
 import { cn } from "@/lib/utils";
 
 export interface InputProps
-  extends React.InputHTMLAttributes<HTMLInputElement> {}
+  extends React.InputHTMLAttributes<HTMLInputElement> {
+  hasError?: string;
+}
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
-  ({ className, type, ...props }, ref) => {
+  ({ className, type, hasError, ...props }, ref) => {
     return (
       <input
         type={type}
         className={cn(
-          "mt-3 flex h-[3.375rem] w-full rounded-[10px] bg-dark-400 px-5 py-[1.0625rem] text-base text-light-200 outline-none file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-white focus-visible:outline-none",
+          "focus: mt-3 flex h-[3.375rem] w-full rounded-[10px] bg-dark-400 px-5 py-[1.0625rem] text-base text-light-200 outline-none file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-white focus:outline-none",
+          {
+            "border-2 border-red": hasError,
+            "ring-offset-2 ring-offset-dark-400 focus:ring-2 focus:ring-light-300":
+              !hasError,
+          },
           className,
         )}
         ref={ref}
