47de99cd1b2b59d82b396caed7a88c179ed23f77
Christoph <christophschmid01@gmail.com>
2024-08-11 14:34:58 +0200

    feat(auth): Add form validation to LogIn page

--- src/pages/_auth/LogIn.tsx
+++ src/pages/_auth/LogIn.tsx
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import { FaEye, FaEyeSlash } from "react-icons/fa";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import { ILoginErrors } from "@/types";
 import OAuth from "@/components/shared/_auth/OAuth";
 
 function LogIn() {
@@ -11,16 +12,61 @@ function LogIn() {
     email: "",
     password: "",
   });
+  const [errors, setErrors] = useState<ILoginErrors>({
+    email: "",
+    password: "",
+  });
+
+  function validateForm() {
+    let isValid = true;
+    const newErrors: ILoginErrors = { email: "", password: "" };
+
+    const { email, password } = formData;
+
+    if (!email) {
+      isValid = false;
+      newErrors.email = "Please enter your email address.";
+    } else if (
+      !/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/.test(formData.email)
+    ) {
+      isValid = false;
+      newErrors.email = "Invalid email format";
+    }
+
+    if (!password) {
+      isValid = false;
+      newErrors.password = "Please enter your password.";
+    }
+
+    setErrors(newErrors);
+    return isValid;
+  }
+
+  function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+
+    if (!validateForm()) return;
+  }
 
   function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
     setFormData({
       ...formData,
       [e.target.id]: e.target.value,
     });
+
+    if (errors[e.target.id as keyof ILoginErrors]) {
+      setErrors({
+        ...errors,
+        [e.target.id]: "",
+      });
+    }
   }
 
   return (
-    <form className="w-full max-w-[400px] px-6 text-light-200 md:px-0">
+    <form
+      onSubmit={handleSubmit}
+      className="w-full max-w-[400px] px-6 text-light-200 md:px-0"
+    >
       <img
         src="/assets/images/logo.svg"
         alt="company logo"
@@ -40,27 +86,43 @@ function LogIn() {
           id="email"
           onChange={handleChange}
           value={formData.email}
+          hasError={errors.email}
         />
+        {errors.email && (
+          <div className="mt-2 flex items-center gap-2">
+            <img src="/assets/icons/error.svg" alt="" />
+            <small className="text-red">{errors.email}</small>
+          </div>
+        )}
       </div>
       <div className="form-control mt-5">
         <label htmlFor="name">Password</label>
         <div className="relative">
-          <Input
-            type={showPassword ? "text" : "password"}
-            id="password"
-            name="name"
-            onChange={handleChange}
-            value={formData.password}
-          />
-          <div
-            onClick={() => setShowPassword(!showPassword)}
-            className="absolute right-4 top-1/2 -translate-y-[50%] cursor-pointer"
-          >
-            {!showPassword ? <FaEye className="" /> : <FaEyeSlash />}
+          <div className="relative">
+            <Input
+              type={showPassword ? "text" : "password"}
+              id="password"
+              name="name"
+              onChange={handleChange}
+              value={formData.password}
+              hasError={errors.password}
+            />
+            <div
+              onClick={() => setShowPassword(!showPassword)}
+              className="absolute right-4 top-1/2 -translate-y-[50%] cursor-pointer"
+            >
+              {!showPassword ? <FaEye className="" /> : <FaEyeSlash />}
+            </div>
           </div>
+          {errors.password && (
+            <div className="mt-2 flex items-center gap-2">
+              <img src="/assets/icons/error.svg" alt="" />
+              <small className="text-red">{errors.password}</small>
+            </div>
+          )}
         </div>
       </div>
-      <div className="mt-[2.625rem]">
+      <div className="mt-[1.875rem]">
         <Button variant="auth" size="md">
           Log In
         </Button>
