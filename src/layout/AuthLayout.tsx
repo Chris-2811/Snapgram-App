b85f8b4907eefd52dcceb009f94b3d5af9551b5c
Christoph <christophschmid01@gmail.com>
2024-08-11 14:54:08 +0200

    feat(auth): Add AuthLayout component
    - Created AuthLayout component to structure authentication pages

--- src/layout/AuthLayout.tsx
+++ src/layout/AuthLayout.tsx
@@ -3,8 +3,15 @@ import { Outlet } from "react-router-dom";
 
 function AuthLayout() {
   return (
-    <div>
-      <Outlet />
+    <div className="flex h-screen flex-col md:flex-row">
+      <div className="bg-dark-100 flex flex-1 items-start justify-center pt-40 lg:items-center lg:pt-0">
+        <Outlet />
+      </div>
+      <img
+        src="/assets/images/side-img.svg"
+        alt=""
+        className="hidden h-screen object-cover md:w-1/2 lg:block"
+      />
     </div>
   );
 }
