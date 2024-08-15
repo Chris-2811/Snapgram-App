e1ccaada814c739e600a2862e8bf43355e2b1454
Christoph <christophschmid01@gmail.com>
2024-08-15 15:13:02 +0200

    feat(auth): Update AuthLayout to handle authenticated users
    - Add redirect to Home if user is authenticated

--- src/layout/AuthLayout.tsx
+++ src/layout/AuthLayout.tsx
@@ -1,18 +1,28 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Outlet } from "react-router-dom";
+import { AuthContext } from "@/context/AuthContext";
+import { Navigate } from "react-router-dom";
 
 function AuthLayout() {
+  const { isAuthenticated } = useContext(AuthContext);
+
   return (
-    <div className="flex h-screen flex-col md:flex-row">
-      <div className="bg-dark-100 flex flex-1 items-start justify-center pt-40 lg:items-center lg:pt-0">
-        <Outlet />
-      </div>
-      <img
-        src="/assets/images/side-img.svg"
-        alt=""
-        className="hidden h-screen object-cover md:w-1/2 lg:block"
-      />
-    </div>
+    <>
+      {isAuthenticated ? (
+        <Navigate to="/" />
+      ) : (
+        <div className="flex h-screen flex-col md:flex-row">
+          <div className="flex flex-1 items-start justify-center bg-dark-100 pt-40 lg:items-center lg:pt-0">
+            <Outlet />
+          </div>
+          <img
+            src="/assets/images/side-img.svg"
+            alt=""
+            className="hidden h-screen object-cover md:w-1/2 lg:block"
+          />
+        </div>
+      )}
+    </>
   );
 }
 
