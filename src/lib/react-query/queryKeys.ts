0f02dfeb3492ea9da87d3a109e8e4470c1551d10
Christoph <christophschmid01@gmail.com>
2024-08-20 18:02:07 +0200

    feat(queries): Add query keys for various API endpoints

---
+++ src/lib/react-query/queryKeys.ts
@@ -0,0 +1,28 @@
+export enum QUERY_KEYS {
+  // AUTH KEYS
+  CREATE_USER_ACCOUNT = "createUserAccount",
+
+  // USER KEYS
+  GET_CURRENT_USER = "getCurrentUser",
+  GET_USERS = "getUsers",
+  GET_USER_BY_ID = "getUserById",
+
+  // POST KEYS
+  GET_POSTS = "getPosts",
+  GET_REELS = "getReels",
+  GET_REELS_BY_ID = "getReelsById",
+  GET_INFINITE_POSTS = "getInfinitePosts",
+  GET_RECENT_POSTS = "getRecentPosts",
+  GET_SAVED_POSTS = "getSavedPosts",
+  GET_POST_BY_ID = "getPostById",
+  GET_POSTS_BY_ID = "getPostsById",
+  GET_POSTS_BY_IDS = "getPostsByIds",
+  GET_USER_POSTS = "getUserPosts",
+  GET_FILE_PREVIEW = "getFilePreview",
+  CHECK_IF_LIKED = "checkIfLiked",
+  GET_LIKES_BY_POST_ID = "getLikesByPostId",
+  GET_COMMENTS_BY_POST_ID = "getCommentsByPostId",
+
+  //  SEARCH KEYS
+  SEARCH_POSTS = "getSearchPosts",
+}
