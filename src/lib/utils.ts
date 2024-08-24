c2090df930c07732f157235673e1ac4769d0f488
Christoph <christophschmid01@gmail.com>
2024-08-24 11:12:41 +0200

    feat(utils): Add formatPostDate function for formatting post dates

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,30 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { Timestamp } from "firebase/firestore";
+import { format, isThisYear } from "date-fns";
+
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function formatPostDate(createdAt: Timestamp | String) {
+  let date: Date;
+  let formattedDate;
+
+  if (createdAt instanceof Timestamp) {
+    date = createdAt.toDate();
+  } else {
+    date = new Date(String(createdAt));
+  }
+
+  if (isThisYear(date)) {
+    // If the date is from the current year, format it as "Jun 18 at 12:13 PM"
+    formattedDate = format(date, "MMM d 'at' h:mm aaa");
+  } else {
+    // If the date is from a different year, format it as "18 Jun 2023"
+    formattedDate = format(date, "d MMM yyyy");
+  }
+
+  return formattedDate.replace(/am|pm/g, (match) => match.toUpperCase());
 }
