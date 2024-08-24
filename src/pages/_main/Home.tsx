1c506dc03bff0ecfd2c115c4ddec8a960b3094b8
Christoph <christophschmid01@gmail.com>
2024-08-24 11:16:07 +0200

    feat(homefeed): Add HomeFeed component to Homepage

--- src/pages/_main/Home.tsx
+++ src/pages/_main/Home.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import TopCreators from "@/components/Home/TopCreators";
+import HomeFeed from "@/components/Home/HomeFeed";
 
 function Home() {
   return (
     <div className="flex justify-between">
-      <div className="min-h-screen flex-1 border border-green-400"></div>
+      <div className="flex min-h-screen flex-1 border border-green-400">
+        <HomeFeed />
+      </div>
       <TopCreators />
     </div>
   );
