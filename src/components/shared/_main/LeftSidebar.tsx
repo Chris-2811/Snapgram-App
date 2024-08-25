56a8d35e0024620eb6070a68bb88bdbd6359f8b0
Christoph <christophschmid01@gmail.com>
2024-08-25 13:48:03 +0200

    feat(leftsidebar): Update LeftSidebar component with user profile link and avatar updates

--- src/components/shared/_main/LeftSidebar.tsx
+++ src/components/shared/_main/LeftSidebar.tsx
@@ -1,15 +1,19 @@
 import { sidebarLinks } from "@/constants/index";
 import { auth } from "@/lib/firebase/firebase";
 import { signOut } from "firebase/auth";
-import { useLocation } from "react-router-dom";
+import { useLocation, Link } from "react-router-dom";
 import { NavLink } from "react-router-dom";
 import { useContext } from "react";
 import { AuthContext } from "@/context/AuthContext";
 import { useNavigate } from "react-router-dom";
 import { Avatar, AvatarImage, AvatarFallback } from "@/components/ui/avatar";
+import { getInitials } from "@/lib/utils";
 
 function LeftSidebar() {
   const { pathname } = useLocation();
+  const { user } = useContext(AuthContext);
+
+  console.log(user);
 
   const navigate = useNavigate();
   const { setIsAuthenticated } = useContext(AuthContext);
@@ -27,6 +31,13 @@ function LeftSidebar() {
     }
   }
 
+  const encodeUsername = (username: string) =>
+    encodeURIComponent(username.toLowerCase());
+
+  const profileIdentifier = user?.username
+    ? encodeUsername(user.username)
+    : user?.userId;
+
   return (
     <div className="sticky top-0 hidden h-screen max-h-[1024px] max-w-[270px] border border-red bg-dark-200 px-6 pb-8 pt-12 text-light-100 lg:flex lg:min-w-[270px] lg:flex-col lg:justify-between">
       <div>
@@ -36,15 +47,30 @@ function LeftSidebar() {
           className="w-[174px]"
         />
         <div className="my-11 flex items-center gap-3">
-          <Avatar className="">
-            <AvatarImage src="/assets/images/avatar.jpg" alt="avatar" />
-            <AvatarFallback>JD</AvatarFallback>
-          </Avatar>
+          <Link to={`/profile/${profileIdentifier}`}>
+            <Avatar className="">
+              {user?.photoUrl ? (
+                <>
+                  <AvatarImage src="/assets/images/avatar.jpg" alt="avatar" />
+                  <AvatarFallback>JD</AvatarFallback>
+                </>
+              ) : (
+                <div className="font- grid h-full w-full place-items-center bg-gray-200 text-lg text-black">
+                  {user && getInitials(user?.name)}
+                </div>
+              )}
+            </Avatar>
+          </Link>
           <div>
-            <p className="text-lg font-bold tracking-[-1px] text-light-200">
-              John Doe
+            <Link to={`/profile/${profileIdentifier}`}>
+              <p className="text-lg font-bold tracking-[-1px] text-light-200">
+                {user?.name}
+              </p>
+            </Link>
+            <p className="text-sm text-light-300">
+              {" "}
+              {!user?.username ? `@${user?.name}` : `@${user?.username}`}
             </p>
-            <p className="text-sm text-light-300"> @JohnDoe </p>
           </div>
         </div>
         <nav aria-label="primary-navigation">
