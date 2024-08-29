23a1852e7764de0b75d76b07f79237136d8ff812
Christoph <christophschmid01@gmail.com>
2024-08-29 16:46:48 +0200

    refactor(CreatePost): Update CreatePost page layout to fix responsive styling issue

--- src/pages/_main/CreatePost.tsx
+++ src/pages/_main/CreatePost.tsx
@@ -4,7 +4,7 @@ import SidebarPost from "@/components/shared/_main/SidebarPost";
 function CreatePost() {
   return (
     <div className="flex">
-      <div className="flex-1 px-4 pb-[7.25rem] pt-10 lg:px-[3rem] lg:pt-[5.4375rem]">
+      <div className="flex-1 px-4 pb-[7.25rem] pt-10 md:px-10 lg:px-[3rem] lg:pt-[5.4375rem]">
         <div className="max-w-[600px]">
           <div className="flex items-center gap-2">
             <img
