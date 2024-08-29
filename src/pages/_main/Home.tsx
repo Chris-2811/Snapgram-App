2a4e35c78c4a85acfa0e9cbcb51bff424acb3e59
Christoph <christophschmid01@gmail.com>
2024-08-29 16:45:53 +0200

    refactor(Home): Update Home page layout to fix border width inconsistency

--- src/pages/_main/Home.tsx
+++ src/pages/_main/Home.tsx
@@ -6,7 +6,7 @@ import StoriesSlider from "@/components/Home/StoriesSlider";
 function Home() {
   return (
     <div className="flex justify-between">
-      <div className="flex min-h-screen flex-1 flex-col border border-green-400 px-4 md:px-10 lg:px-[3.25rem] xl:px-[5rem] 3xl:px-[6rem]">
+      <div className="flex min-h-screen flex-1 flex-col border border-green-400 px-4 md:px-10 lg:px-[3.25rem] xl:px-[4rem] 3xl:px-[6rem]">
         <StoriesSlider />
         <HomeFeed />
       </div>
