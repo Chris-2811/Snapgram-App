fca771d34d53f6aa6df3d1bd05c7a5deded32f6b
Christoph <christophschmid01@gmail.com>
2024-08-29 13:20:50 +0200

    feat(filter): Add Filter component to shared main components

---
+++ src/components/shared/_main/Filter.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Filter() {
+  return (
+    <div>
+      <img
+        src="/assets/icons/filter.svg"
+        alt="filter-icon"
+        className="h-4 w-4 cursor-pointer lg:h-5 lg:w-5"
+      />
+    </div>
+  );
+}
+
+export default Filter;
