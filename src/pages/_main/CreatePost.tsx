99194503cb9e2c7e8c726d9138fa0b92eed533df
Christoph <christophschmid01@gmail.com>
2024-08-29 15:11:29 +0200

    feat(createpost): Update CreatePost page with SidebarPost component

--- src/pages/_main/CreatePost.tsx
+++ src/pages/_main/CreatePost.tsx
@@ -1,18 +1,24 @@
 import PostForm from "../../components/forms/PostForm";
+import SidebarPost from "@/components/shared/_main/SidebarPost";
 
 function CreatePost() {
   return (
-    <div className="px-4 pb-[7.25rem] pt-10 md:px-8 lg:px-[3.75rem] lg:pt-[5.4375rem]">
-      <div className="max-w-[600px]">
-        <div className="flex items-center gap-2">
-          <img
-            src="/assets/icons/gallery-add.svg"
-            alt=""
-            className="invert-white w-9"
-          />
-          <h1 className="heading-lg">Create a Post</h1>
+    <div className="flex">
+      <div className="flex-1 px-4 pb-[7.25rem] pt-10 lg:px-[3rem] lg:pt-[5.4375rem]">
+        <div className="max-w-[600px]">
+          <div className="flex items-center gap-2">
+            <img
+              src="/assets/icons/gallery-add.svg"
+              alt=""
+              className="invert-white w-9"
+            />
+            <h1 className="heading-md lg:heading-lg">Create a Post</h1>
+          </div>
+          <PostForm action="create" />
         </div>
-        <PostForm action="create" />
+      </div>
+      <div>
+        <SidebarPost />
       </div>
     </div>
   );
