59a8ba3242854a5d98079fbee4160606ded9aed5
Christoph <christophschmid01@gmail.com>
2024-09-11 16:52:09 +0200

    feat(loader): Add Loader component for displaying loading state

---
+++ src/components/shared/Loader.tsx
@@ -0,0 +1,13 @@
+const Loader = () => (
+  <div>
+    <img
+      src="/assets/icons/loader.svg"
+      alt="loader"
+      width={20}
+      height={20}
+      className="animate-spin"
+    />
+  </div>
+);
+
+export default Loader;
