7f886f09eadba6b525360338248b5941e67b2842
Christoph <christophschmid01@gmail.com>
2024-09-13 14:11:29 +0200

    feat(Searchbar): Add Searchbar component

---
+++ src/components/shared/_main/Searchbar.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Input } from "@/components/ui/input";
+
+function Searchbar() {
+  return (
+    <div className="relative">
+      <img
+        src="/assets/icons/search.svg"
+        alt="search-icon"
+        className="absolute left-5 top-1/2 -translate-y-1/2"
+      />
+      <Input
+        placeholder="Search"
+        className="pl-[3.75rem] text-lg placeholder:text-light-400"
+      />
+    </div>
+  );
+}
+
+export default Searchbar;
