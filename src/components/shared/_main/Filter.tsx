8968a80fde1ef4f235bc7768ecce93b23f0336d7
Christoph <christophschmid01@gmail.com>
2024-09-14 14:37:31 +0200

    feat(Filter): Update Filter component UI and styles

--- src/components/shared/_main/Filter.tsx
+++ src/components/shared/_main/Filter.tsx
@@ -3,11 +3,14 @@ import React from "react";
 function Filter() {
   return (
     <div>
-      <img
-        src="/assets/icons/filter.svg"
-        alt="filter-icon"
-        className="h-4 w-4 cursor-pointer lg:h-5 lg:w-5"
-      />
+      <div className="flex items-center gap-[0.625rem] rounded-[14px] bg-dark-400 px-4 py-3">
+        <p className="text-xs font-medium">All</p>
+        <img
+          src="/assets/icons/filter.svg"
+          alt="filter-icon"
+          className="h-4 w-4 cursor-pointer lg:h-5 lg:w-5"
+        />
+      </div>
     </div>
   );
 }
