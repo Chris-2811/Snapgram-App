f9410f22327b31b09bfda9a5c7a99693078537ec
Christoph <christophschmid01@gmail.com>
2024-09-17 12:16:33 +0200

    feat(postdetails): Update PostDetails component

--- src/components/shared/_main/PostDetails.tsx
+++ src/components/shared/_main/PostDetails.tsx
@@ -8,13 +8,29 @@ import { QUERY_KEYS } from "@/lib/react-query/queryKeys";
 import { getUserById } from "@/lib/firebase/api";
 import { useQueries } from "@tanstack/react-query";
 import { AuthContext } from "@/context/AuthContext";
+import { IPost } from "@/types";
+import { useSwipeable } from "react-swipeable";
 
-function PostDetails({ handleCloseModal, currentPost, creator }) {
+interface PostDetailsProps {
+  handleCloseModal: (e: React.MouseEvent) => void;
+  currentPost: IPost;
+  creator: any;
+  imageUrls: string[];
+}
+
+function PostDetails({
+  handleCloseModal,
+  currentPost,
+  creator,
+  imageUrls,
+}: PostDetailsProps) {
   const [text, setText] = useState("");
   const [isCaptionExpened, setIsCaptionExpended] = useState<boolean>(false);
+  const [isImageLoaded, setIsImageLoaded] = useState<boolean>(false);
   const { data: comments, refetch: refetchComments } = useGetCommentsByPostId(
     currentPost.postId,
   );
+  const [activePhotoIndex, setActivePhotoIndex] = useState<number>(0);
   const recentComments = comments?.slice(0, 3);
   console.log("recentComments", recentComments);
   const userQueries = useQueries({
@@ -50,23 +66,54 @@ function PostDetails({ handleCloseModal, currentPost, creator }) {
     setText(e.target.value);
   }
 
+  const swipeHandlers = useSwipeable({
+    onSwipedLeft: () => {
+      setActivePhotoIndex((prev) => (prev + 1) % imageUrls.length);
+    },
+    onSwipedRight: () => {
+      setActivePhotoIndex((prev) => (prev - 1) % imageUrls.length);
+    },
+    trackMouse: true,
+  });
+
   const captionPreview = `${currentPost.caption.slice(0, 100)}...`;
+
+  console.log(imageUrls);
+
+  const filteredTags =
+    currentPost.tags.length > 3
+      ? currentPost.tags.filter((_, index) => (index + 1) % 3 === 0)
+      : currentPost.tags;
 
   return (
     <div>
       <div
         onClick={handleCloseModal}
-        className="fixed inset-0 flex items-center justify-center bg-[#202020] opacity-60"
+        className="fixed inset-0 bg-[#202020] opacity-60"
       ></div>
-      <div className="fixed left-1/2 top-1/2 flex min-h-[600px] -translate-x-1/2 -translate-y-1/2 overflow-hidden rounded-[30px] bg-dark-200 xl:h-[582px] xl:w-[1110px]">
-        <img
-          src={currentPost.photoUrls[0]}
-          alt=""
-          className="min-h-full xl:w-[510px]"
-        />
-        <div className="flex flex-col px-7 pb-[1.875rem] pt-9 xl:w-[600px]">
-          <div className="flex justify-between">
-            <div className="">
+
+      {isImageLoaded && (
+        <div className="fixed left-1/2 top-1/2 hidden h-[500px] w-full -translate-x-1/2 -translate-y-1/2 justify-center overflow-hidden rounded-[30px] px-5 lg:flex lg:max-w-[900px] xl:h-[582px] xl:max-w-[1120px] 2xl:max-w-none">
+          <div className="relative w-[45%] overflow-hidden rounded-l-[30px] xl:w-[509px]">
+            <img
+              src={imageUrls[activePhotoIndex]}
+              alt=""
+              className="h-full w-full object-cover"
+              {...swipeHandlers}
+            />
+            <div className="absolute bottom-[1.125rem] left-1/2 flex justify-center gap-2">
+              {imageUrls.length > 1 &&
+                imageUrls.map((image, index) => (
+                  <div
+                    key={index}
+                    onClick={() => setActivePhotoIndex(index)}
+                    className={`h-[0.6125rem] w-[0.6125rem] cursor-pointer rounded-full ${index === activePhotoIndex ? "bg-white" : "bg-light-400"}`}
+                  ></div>
+                ))}
+            </div>
+          </div>
+          <div className="flex w-[55%] flex-col rounded-r-[30px] bg-dark-200 px-7 pb-[1.875rem] pt-9 xl:w-[600px]">
+            <div className="flex w-full justify-between">
               <div className="flex items-center gap-3">
                 <img
                   src={`${
@@ -86,123 +133,130 @@ function PostDetails({ handleCloseModal, currentPost, creator }) {
                   </small>
                 </div>
               </div>
-            </div>
-            <div className="flex gap-3">
-              <img
-                src="/assets/icons/edit.svg"
-                alt="edit"
-                className="w-5 cursor-pointer"
-              />
-              <img
-                src="/assets/icons/delete.svg"
-                alt="delete"
-                className="w-5 cursor-pointer"
-              />
-            </div>
-          </div>
-          <div className="mt-5">
-            <div>
-              <div className="max-w-lg font-semibold">
-                {isCaptionExpened ? currentPost.caption : captionPreview}
-                <span
-                  onClick={() => setIsCaptionExpended(!isCaptionExpened)}
-                  className="ml-2 cursor-pointer whitespace-nowrap text-xs text-light-300"
-                >
-                  {isCaptionExpened ? "show less" : "show more"}
-                </span>
+              <div className="flex gap-3">
+                <img
+                  src="/assets/icons/edit.svg"
+                  alt="edit"
+                  className="w-5 cursor-pointer"
+                />
+                <img
+                  src="/assets/icons/delete.svg"
+                  alt="delete"
+                  className="w-5 cursor-pointer"
+                />
               </div>
+            </div>
+            <div className="mt-5">
+              <div>
+                <div className="max-w-lg font-semibold">
+                  {isCaptionExpened ? currentPost.caption : captionPreview}
+                  <span
+                    onClick={() => setIsCaptionExpended(!isCaptionExpened)}
+                    className="ml-2 cursor-pointer whitespace-nowrap text-xs text-light-300"
+                  >
+                    {isCaptionExpened ? "show less" : "show more"}
+                  </span>
+                </div>
 
-              <span
-                className={`flex gap-2 text-light-400 ${
-                  currentPost.caption.length > 50 && "mt-2"
-                }`}
-              >
-                {currentPost.tags
-                  .slice(0, 3)
-                  .map((tag: string, index: number) => (
+                <span
+                  className={`flex gap-2 text-light-400 ${
+                    currentPost.caption.length > 50 && "mt-2"
+                  }`}
+                >
+                  {filteredTags.map((tag: string, index: number) => (
                     <p key={index}>#{tag}</p>
                   ))}
-              </span>
+                </span>
+              </div>
             </div>
-          </div>
 
-          {/* Comments */}
-          <div className="flex flex-1 flex-col justify-between">
-            <div className="mt-[1.875rem] space-y-[1.875rem] border-t border-t-dark-400 lg:pt-[1.75rem]">
-              {recentComments?.map((item, index) => (
-                <div className="flex items-start justify-between">
-                  <div className="flex gap-2">
-                    <img
-                      src={
-                        userQueries[index].data?.photoUrl
-                          ? userQueries[index].data?.photoUrl
-                          : "/assets/images/profile.png"
-                      }
-                      alt="avatar"
-                      width={36}
-                      className="h-[36px] rounded-full"
-                    />
-                    <div className="">
-                      <div className="flex items-center gap-[0.625rem]">
-                        <p className="text-xs font-medium lg:max-w-[376px]">
-                          <h3 className="mr-3 inline text-sm font-semibold tracking-[-1px] text-light-300">
-                            {userQueries[index].data?.username
-                              ? userQueries[index].data?.username
-                              : userQueries[index].data?.name}
-                          </h3>
-                          {item.text}
-                        </p>
-                      </div>
-                      <div className="mt-1 flex items-center gap-[0.6875rem]">
-                        <div className="text-[0.625rem] text-light-300">1d</div>
-                        <div className="flex items-center gap-[0.1875rem]">
-                          <img
-                            src="/assets/icons/reply.svg"
-                            alt="like-comment"
-                            width={16}
-                          />
-                          <p className="text-[10px]">Reply</p>
+            {/* Comments */}
+            <div className="flex flex-1 flex-col justify-between">
+              <div className="mt-[1.875rem] space-y-[1.875rem] border-t border-t-dark-400 lg:pt-[1.75rem]">
+                {recentComments?.map((item, index) => (
+                  <div className="flex items-start justify-between">
+                    <div className="flex gap-2">
+                      <img
+                        src={
+                          userQueries[index].data?.photoUrl
+                            ? userQueries[index].data?.photoUrl
+                            : "/assets/images/profile.png"
+                        }
+                        alt="avatar"
+                        width={36}
+                        className="h-[36px] rounded-full"
+                      />
+                      <div className="">
+                        <div className="flex items-center gap-[0.625rem]">
+                          <p className="text-xs font-medium lg:max-w-[376px]">
+                            <h3 className="mr-3 inline text-sm font-semibold tracking-[-1px] text-light-300">
+                              {userQueries[index].data?.username
+                                ? userQueries[index].data?.username
+                                : userQueries[index].data?.name}
+                            </h3>
+                            {item.text}
+                          </p>
+                        </div>
+                        <div className="mt-1 flex items-center gap-[0.6875rem]">
+                          <div className="text-[0.625rem] text-light-300">
+                            1d
+                          </div>
+                          <div className="flex items-center gap-[0.1875rem]">
+                            <img
+                              src="/assets/icons/reply.svg"
+                              alt="like-comment"
+                              width={16}
+                            />
+                            <p className="text-[10px]">Reply</p>
+                          </div>
                         </div>
                       </div>
                     </div>
+                    <div className="flex items-center gap-2">
+                      <img
+                        src="/assets/icons/like.svg"
+                        alt="like-post"
+                        width={16}
+                      />
+                      <p className="text-[10px] text-light-300">4 likes</p>
+                    </div>
                   </div>
-                  <div className="flex items-center gap-2">
-                    <img
-                      src="/assets/icons/like.svg"
-                      alt="like-post"
-                      width={16}
-                    />
-                    <p className="text-[10px] text-light-300">4 likes</p>
-                  </div>
-                </div>
-              ))}
-            </div>
-            <div className="">
-              <form
-                onSubmit={handleSubmit}
-                className="mt-10 flex items-center gap-4"
-              >
-                <img
-                  src="/assets/images/profile.png"
-                  alt="profile picture"
-                  className="h-10 w-10"
-                />
-                <div className="form-control relative w-full">
-                  <Input
-                    className="mt-0 h-11 bg-dark-400 placeholder:text-light-400"
-                    placeholder="Write your comment..."
-                    onChange={handleTextChange}
-                    value={text}
+                ))}
+              </div>
+              <div className="">
+                <form
+                  onSubmit={handleSubmit}
+                  className="mt-10 flex items-center gap-4"
+                >
+                  <img
+                    src="/assets/images/profile.png"
+                    alt="profile picture"
+                    className="h-10 w-10"
                   />
-                  <button className="absolute right-4 top-1/2 -translate-y-1/2">
-                    <img src="/assets/icons/plain.svg" alt="" className="" />
-                  </button>
-                </div>
-              </form>
+                  <div className="form-control relative w-full">
+                    <Input
+                      className="mt-0 h-11 bg-dark-400 placeholder:text-light-400"
+                      placeholder="Write your comment..."
+                      onChange={handleTextChange}
+                      value={text}
+                    />
+                    <button className="absolute right-4 top-1/2 -translate-y-1/2">
+                      <img src="/assets/icons/plain.svg" alt="" className="" />
+                    </button>
+                  </div>
+                </form>
+              </div>
             </div>
           </div>
         </div>
-      </div>
+      )}
+
+      <img
+        src={imageUrls[0]}
+        alt=""
+        style={{ display: "none" }}
+        onLoad={() => setIsImageLoaded(true)}
+      />
     </div>
   );
 }
