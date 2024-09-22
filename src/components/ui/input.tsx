307c97617e90d773bf007aa4a4023ee2990d6fee
Christoph <christophschmid01@gmail.com>
2024-09-22 14:04:03 +0200

    feat(postdetails): Update PostDetails component with comment length validation

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -13,7 +13,7 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
       <input
         type={type}
         className={cn(
-          "focus: mt-3 flex h-[3.375rem] w-full rounded-[10px] bg-dark-400 px-5 py-[1.0625rem] text-base text-light-200 outline-none file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-white focus:outline-none",
+          "flex h-[3.375rem] w-full rounded-[10px] bg-dark-400 px-5 py-[1.0625rem] text-base text-light-200 outline-none file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-white focus:outline-none",
           {
             "border-2 border-red": hasError,
             "ring-offset-2 ring-offset-dark-400 focus:ring-2 focus:ring-light-300":
