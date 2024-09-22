d4ecfb47920a71e3c6d2252e46e156be23cf3ba8
Christoph <christophschmid01@gmail.com>
2024-09-22 17:00:02 +0200

    feat(poststats): Add save and delete functionality for posts
    -  Add checkIfPostIsSaved function
    - Add savePost function to api.ts
    - Add useSavePost to mutations.ts
    - Add deleteSavedPost function to api.ts

--- src/lib/react-query/mutations.ts
+++ src/lib/react-query/mutations.ts
@@ -1,7 +1,13 @@
-import { createUserAccount, logInAccount, saveUserToDB } from "../firebase/api";
-import { useMutation } from "@tanstack/react-query";
-import { INewUser } from "@/types";
+import {
+  createUserAccount,
+  logInAccount,
+  saveUserToDB,
+  savePost,
+} from "../firebase/api";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { INewUser, IPost } from "@/types";
 import { Timestamp } from "firebase/firestore";
+import { QUERY_KEYS } from "./queryKeys";
 
 export const useCreateUserAccount = () => {
   return useMutation({
@@ -27,5 +33,17 @@ export const useSaveUserToDB = () => {
       name: string;
       createdAt: Timestamp;
     }) => saveUserToDB(user),
+  });
+};
+
+export const useSavePost = () => {
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: (post: IPost) => savePost(post),
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: [QUERY_KEYS.GET_POSTS],
+      });
+    },
   });
 };
