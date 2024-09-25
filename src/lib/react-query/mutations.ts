9d4fbc05e29d3040dae36886ca55fe422eff4cf5
Christoph <christophschmid01@gmail.com>
2024-09-25 12:32:30 +0200

    feat(mutation): Add useLikePost hook to mutations.ts

--- src/lib/react-query/mutations.ts
+++ src/lib/react-query/mutations.ts
@@ -3,6 +3,7 @@ import {
   logInAccount,
   saveUserToDB,
   savePost,
+  likePost,
 } from "../firebase/api";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { INewUser, IPost } from "@/types";
@@ -41,9 +42,26 @@ export const useSavePost = () => {
   return useMutation({
     mutationFn: ({ postId, userId }: { postId: string; userId: string }) =>
       savePost(postId, userId),
+export const useLikePost = () => {
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: ({
+      postId,
+      likesArray,
+    }: {
+      postId: string;
+      likesArray: string[];
+    }) => likePost(postId, likesArray),
     onSuccess: () => {
       queryClient.invalidateQueries({
         queryKey: [QUERY_KEYS.GET_POSTS],
+      });
+      queryClient.invalidateQueries({
+        queryKey: [QUERY_KEYS.GET_POSTS_BY_ID],
+      });
+
+      queryClient.invalidateQueries({
+        queryKey: [QUERY_KEYS.GET_POSTS_BY_IDS],
       });
     },
   });
