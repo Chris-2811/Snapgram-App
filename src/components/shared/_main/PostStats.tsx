581d980877bf9e9bfeb7047cfdae5358dd89c88a
Christoph <christophschmid01@gmail.com>
2024-10-03 14:59:06 +0200

    feat(poststats): Add ShareSocial component for sharing posts

--- src/components/shared/_main/PostStats.tsx
+++ src/components/shared/_main/PostStats.tsx
@@ -7,6 +7,7 @@ import { AuthContext } from "@/context/AuthContext";
 import { IPost } from "@/types/index";
 import { checkIsLiked } from "@/lib/utils";
 import { useDeleteSavedPost } from "@/lib/react-query/mutations";
+import { ShareSocial } from "react-share-social";
 
 type PostStatsProps = {
   post: IPost;
@@ -19,6 +20,8 @@ function PostStats({ post, comments }: PostStatsProps) {
   const [isLiked, setIsLiked] = useState<boolean>();
   const [isSaved, setIsSaved] = useState<boolean>(false);
   const [docId, setDocId] = useState<String | null>();
+  const [showShareModal, setShowShareModal] = useState<boolean>(false);
+
   const { mutate: savePost } = useSavePost();
   const { mutate: likePost } = useLikePost();
   const { mutate: deleteSavedPost } = useDeleteSavedPost();
@@ -52,6 +55,12 @@ function PostStats({ post, comments }: PostStatsProps) {
     }
   }
 
+  function handleSharePost(e: React.MouseEvent<HTMLImageElement>) {
+    e.stopPropagation();
+
+    setShowShareModal(true);
+  }
+
   function handleLikePost(e: React.MouseEvent<HTMLParagraphElement>) {
     e.stopPropagation();
 
@@ -68,38 +77,77 @@ function PostStats({ post, comments }: PostStatsProps) {
   }
 
   return (
-    <div className="flex items-center justify-between">
-      <div className="flex justify-between gap-[1.875rem]">
-        <div className="flex items-center gap-[0.375rem]">
+    <>
+      <div className="flex items-center justify-between">
+        <div className="flex justify-between gap-[1.875rem]">
+          <div className="flex items-center gap-[0.375rem]">
+            <img
+              src={
+                checkIsLiked(likes, user.userId)
+                  ? "/assets/icons/liked.svg"
+                  : "/assets/icons/like.svg"
+              }
+              alt=""
+              className="w-5 cursor-pointer"
+              onClick={handleLikePost}
+            />
+            <p className="min-w-[10px]">{likes.length}</p>
+          </div>
+          <div className="flex items-center gap-[0.375rem]">
+            <img src="/assets/icons/chat.svg" alt="" />
+            <p className="min-w-[10px]">{comments ? comments.length : "0"}</p>
+          </div>
+          <div className="flex cursor-pointer items-center gap-[0.375rem]">
+            <img
+              src="/assets/icons/share.svg"
+              alt="share-icon"
+              onClick={handleSharePost}
+            />
+
+            <p className="min-w-[10px]">{post.shares ? post.shares : "0"}</p>
+          </div>
+        </div>
+        <div className="cursor-pointer" onClick={handleSavePost}>
           <img
-            src={
-              checkIsLiked(likes, user.userId)
-                ? "/assets/icons/liked.svg"
-                : "/assets/icons/like.svg"
-            }
+            src={isSaved ? "/assets/icons/saved.svg" : "/assets/icons/save.svg"}
             alt=""
-            className="w-5 cursor-pointer"
-            onClick={handleLikePost}
+            className="w-5"
           />
-          <p className="min-w-[10px]">{likes.length}</p>
-        </div>
-        <div className="flex items-center gap-[0.375rem]">
-          <img src="/assets/icons/chat.svg" alt="" />
-          <p className="min-w-[10px]">{comments ? comments.length : "0"}</p>
-        </div>
-        <div className="flex cursor-pointer items-center gap-[0.375rem]">
-          <img src="/assets/icons/share.svg" alt="share-icon" />
-          <p className="min-w-[10px]">{post.shares ? post.shares : "0"}</p>
         </div>
       </div>
-      <div className="cursor-pointer" onClick={handleSavePost}>
-        <img
-          src={isSaved ? "/assets/icons/saved.svg" : "/assets/icons/save.svg"}
-          alt=""
-          className="w-5"
-        />
-      </div>
-    </div>
+      {/*  {showShareModal && (
+        <div className="fixed inset-0 z-[100] grid grid-cols-1 items-end justify-items-center">
+          <ShareSocial
+            url="localhost.com"
+            socialTypes={[
+              "facebook",
+              "twitter",
+              "linkedin",
+              "whatsapp",
+              "email",
+            ]}
+            style={{
+              root: {
+                background: "linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)",
+                borderRadius: 3,
+                border: 0,
+                boxShadow: "0 3px 5px 2px rgba(255, 105, 135, .3)",
+                color: "white",
+                width: "100%",
+                paddingTop: "0",
+                paddingBottom: "20px",
+              },
+              copyContainer: {
+                border: "1px solid blue",
+
+                background: "white",
+                width: "100%",
+              },
+            }}
+          />
+        </div>
+      )} */}
+    </>
   );
 }
 
