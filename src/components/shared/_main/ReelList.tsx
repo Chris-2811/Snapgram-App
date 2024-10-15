9e72d52c5aa1fbbef0e6010167b282d2705b536e
Christoph <christophschmid01@gmail.com>
2024-10-15 15:02:38 +0200

    feat(reellist): Update ReelList component to render a grid of reels

--- src/components/shared/_main/ReelList.tsx
+++ src/components/shared/_main/ReelList.tsx
@@ -1,8 +1,15 @@
 import React from "react";
 import { IReel } from "@/types";
+import Reel from "./Reel";
 
 function ReelList({ reels }: { reels: IReel[] }) {
-  return <div>ReelList</div>;
+  return (
+    <div className="grid justify-center gap-y-5 md:gap-y-6 lg:grid-cols-[repeat(auto-fill,340px)] lg:justify-start lg:gap-x-6">
+      {reels.map((reel) => (
+        <Reel reel={reel} />
+      ))}
+    </div>
+  );
 }
 
 export default ReelList;
