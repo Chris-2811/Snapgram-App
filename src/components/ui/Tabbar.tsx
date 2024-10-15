3097a20a59e3ee5a24045cf2fb99faa8e5fe8076
Christoph <christophschmid01@gmail.com>
2024-10-15 15:05:03 +0200

    refactor(tabbar): Update Tabbar component to improve code readability and maintainability

--- src/components/ui/Tabbar.tsx
+++ src/components/ui/Tabbar.tsx
@@ -18,7 +18,7 @@ function Tabbar({
     <div className="flex items-center justify-between">
       <div className="flex w-full items-center gap-0 overflow-hidden rounded-[10px] border border-light-400 border-opacity-50 bg-dark-300 sm:w-auto sm:max-w-md">
         {pathname === "/reels" ? (
-          <h1>hello reels</h1>
+          <></>
         ) : (
           <>
             <div
@@ -26,7 +26,7 @@ function Tabbar({
               onClick={handleTabClick}
               className={`${
                 isActive === "posts" && "bg-dark-400"
-              } flex w-1/3 cursor-pointer items-center justify-center gap-2 border-r border-r-light-400 border-opacity-40 py-3 xs:px-5 sm:w-auto sm:px-6 md:gap-[0.625rem] md:px-10 xl:px-[3.125rem]`}
+              } flex w-1/3 cursor-pointer items-center justify-center gap-2 text-nowrap border-r border-r-light-400 border-opacity-40 px-3 py-3 xs:px-5 sm:w-auto sm:px-6 md:gap-[0.625rem] md:px-10 xl:px-[3.125rem]`}
             >
               <img
                 src="/assets/icons/posts.svg"
@@ -42,7 +42,7 @@ function Tabbar({
               onClick={handleTabClick}
               className={`${
                 isActive === "reels" && "bg-dark-400"
-              } flex w-1/3 cursor-pointer items-center justify-center gap-2 py-3 xs:px-5 sm:w-auto sm:px-6 md:gap-[0.625rem] md:px-10 xl:px-[3.125rem]`}
+              } flex w-1/3 cursor-pointer items-center justify-center gap-2 text-nowrap px-3 py-3 xs:px-5 sm:w-auto sm:px-6 md:gap-[0.625rem] md:px-10 xl:px-[3.125rem]`}
             >
               <img
                 src="/assets/icons/play.svg"
@@ -59,7 +59,7 @@ function Tabbar({
                 onClick={handleTabClick}
                 className={`${
                   isActive === "collections" && "bg-dark-400"
-                } flex w-1/3 cursor-pointer items-center justify-center gap-2 border-l border-l-light-400 border-opacity-50 py-3 xs:px-5 sm:w-auto sm:px-6 md:gap-[0.625rem] md:px-10 xl:px-[3.125rem]`}
+                } flex w-1/3 cursor-pointer items-center justify-center gap-2 text-nowrap border-l border-l-light-400 border-opacity-50 px-3 py-3 xs:px-5 sm:w-auto sm:px-6 md:gap-[0.625rem] md:px-10 xl:px-[3.125rem]`}
               >
                 <img
                   src="/assets/icons/folder-favorite.svg"
@@ -76,7 +76,7 @@ function Tabbar({
                 onClick={handleTabClick}
                 className={`${
                   isActive === "collections" && "bg-dark-400"
-                } flex w-1/3 cursor-pointer items-center justify-center gap-2 border-l border-l-light-400 border-opacity-50 py-3 xs:px-5 sm:w-auto sm:px-6 md:gap-[0.625rem] md:px-10 xl:px-[3.125rem]`}
+                } flex w-1/3 cursor-pointer items-center justify-center gap-2 text-nowrap border-l border-l-light-400 border-opacity-50 px-3 py-3 xs:px-5 sm:w-auto sm:px-6 md:gap-[0.625rem] md:px-10 xl:px-[3.125rem]`}
               >
                 <img
                   src="/assets/icons/tag.svg"
