a42567d7ce2271d2cb65172e37b61a2e8a01939f
Christoph <christophschmid01@gmail.com>
2024-10-15 15:04:22 +0200

    feat(filter): Update Filter component to accept a className prop with cn()

--- src/components/shared/_main/Filter.tsx
+++ src/components/shared/_main/Filter.tsx
@@ -1,10 +1,16 @@
 import React from "react";
+import { cn } from "@/lib/utils";
 
-function Filter() {
+function Filter({ className }: { className?: string }) {
   return (
     <div>
-      <div className="flex max-w-20 items-center gap-[0.625rem] rounded-[14px] bg-dark-400 px-4 py-3">
-        <p className="text-xs font-medium">All</p>
+      <div
+        className={cn(
+          "flex max-w-20 items-center gap-[0.625rem] rounded-[14px] bg-dark-400 px-4 py-3 text-xs font-medium",
+          className,
+        )}
+      >
+        <p className="">All</p>
         <img
           src="/assets/icons/filter.svg"
           alt="filter-icon"
