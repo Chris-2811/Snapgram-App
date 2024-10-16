4e676fd8a6cea2581bd2b4863e9109e0250cca97
Christoph <christophschmid01@gmail.com>
2024-10-16 14:09:45 +0200

    feat(reelpreview): Add ReelPreview component for displaying reel previews

---
+++ src/components/shared/_main/ReelPreview.tsx
@@ -0,0 +1,40 @@
+import React, { useContext } from "react";
+import { cn } from "@/lib/utils";
+import { Link } from "react-router-dom";
+import { AuthContext } from "@/context/AuthContext";
+
+function ReelPreview({ reel, className }: { reel: any; className?: string }) {
+  const { user } = useContext(AuthContext);
+
+  return (
+    <div
+      className={cn(
+        "relative aspect-[17/30] cursor-pointer overflow-hidden rounded-[30px] border border-light-300/60",
+        className,
+      )}
+    >
+      <video src={reel.videoUrl} alt="" className="w-full" />
+      <div className="absolute right-4 top-4">
+        <img
+          src="assets/icons/play.svg"
+          alt="play-icon"
+          className="invert-white w-6"
+        />
+      </div>
+      <div className="group absolute inset-0 flex items-center justify-center hover:bg-dark-300/50">
+        <div className="hidden items-center gap-6 group-hover:flex">
+          <div className="flex gap-1">
+            <img src="/assets/icons/liked.svg" alt="like-icon" />
+            <p>7.135</p>
+          </div>
+          <div className="flex gap-1">
+            <img src="/assets/icons/chat.svg" alt="chat-icon" />
+            <p>134</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ReelPreview;
