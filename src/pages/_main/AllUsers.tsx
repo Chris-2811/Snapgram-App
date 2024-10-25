5b164bfa7e7959ebf5ca46a1ceddad63581261de
Christoph <christophschmid01@gmail.com>
2024-10-25 16:32:46 +0200

    feat(allusers): Update AllUsers page to include follow change handler

--- src/pages/_main/AllUsers.tsx
+++ src/pages/_main/AllUsers.tsx
@@ -1,11 +1,25 @@
 import UserCard from "@/components/shared/_main/UserCard";
-import { useGetUsers } from "@/lib/react-query/queries";
+import { useGetCurrentUser, useGetUsers } from "@/lib/react-query/queries";
 import { Button } from "@/components/ui/button";
+import { useContext } from "react";
+import { AuthContext } from "@/context/AuthContext";
 
 function AllUsers() {
-  const { data: users, fetchNextPage, hasNextPage } = useGetUsers();
+  const {
+    data: users,
+    fetchNextPage,
+    hasNextPage,
+    refetch: refetchUsers,
+  } = useGetUsers();
+  const { user } = useContext(AuthContext);
+  const { data: currentUser } = useGetCurrentUser(user?.uid);
 
   const allUsers = users?.pages.flatMap((page) => page.map((user) => user));
+
+  const handleFollowChange = () => {
+    console.log("handleFollowChange");
+    refetchUsers();
+  };
 
   return (
     <div className="flex-1 px-[3.75rem] py-20">
@@ -14,7 +28,13 @@ function AllUsers() {
         <h1 className="heading-lg">All Users</h1>
       </div>
       <div className="mt-10 flex flex-wrap gap-12">
-        {allUsers?.map((user) => <UserCard creator={user} size="lg" />)}
+        {allUsers?.map((user) => (
+          <UserCard
+            creator={user}
+            size="lg"
+            handleFollowChange={handleFollowChange}
+          />
+        ))}
       </div>
       {hasNextPage && (
         <div className="flex justify-center">
